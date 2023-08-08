0ea198bfc535a8d1c2618f0891caf9311dafd46b
pandarubrum <cenere.piuma@gmail.com>
2023-08-08 02:04:24 +0200

    Beautify HISTTIMEFORMAT

--- bc_wrapper.sh
+++ bc_wrapper.sh
@@ -77,7 +77,7 @@ HISTFILE=~/.bc_history
 HISTCONTROL='ignoredups:ignorespace'
 HISTSIZE=1000
 HISTFILESIZE=2000
-HISTTIMEFORMAT=$'\033[1;31m%T\033[33m %d/%m/%y ⮕\033[39m  '
+HISTTIMEFORMAT=$'\033[1;38;5;155m%F\033[38;5;147m %T\033[38;5;137m ⮕\033[39m  '
 
 # Called by `readline` when <ALT><V> send to `read`. It checks which PS is used
 # last (PS_READY or PS_BUSY). This is a way to find out if background `coproc`
